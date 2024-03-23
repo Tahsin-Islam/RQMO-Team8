398b1ef4ccfc855c18b996055b4d9f3ad0276e56
Tahsin-Islam <tahsin_i12@outlook.com>
2024-03-23 17:43:23 -0400

    Added more navigation features

--- rare-quest-app/src/topBar.js
+++ rare-quest-app/src/topBar.js
@@ -3,13 +3,17 @@ import './css/topBar.css';
 function TopBar(){
     return(
         <div className="topBar">
-        <div className="companyName">Rare<b>Quest</b></div>
-        <div className="topBar-middle">
-            <a href="#">Home</a>
-            <a href="#">How To Play</a>
-            <a href="#">Contact Us</a>
+            <div className="companyName">Rare<span className='quest'>Quest</span></div>
+            <div className="topBar-middle">
+                <a href="#">Home</a>
+                <a href="#">How To Play</a>
+                <a href="#">Contact Us</a>
+            </div>
+            <div className='topBar-right'>
+                <a className='login'>Login</a>
+                <a className='signup'>Sign Up</a>
+            </div>
         </div>
-    </div>
     )
 }
 
