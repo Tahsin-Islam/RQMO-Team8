8c81e9b1e6fd171d8ac986fed468c86eb630a540
Tahsin-Islam <tahsin_i12@outlook.com>
2024-03-24 17:11:35 -0400

    Added Game Patient Profile

--- rare-quest-app/src/Game.js
+++ rare-quest-app/src/Game.js
@@ -1,7 +1,26 @@
+// Game.js
+
+import React from 'react';
+import './css/Game.css';
+import femalePatientAsset from './Assets/avatar-svgrepo-com 1.png';
+
 function Game() {
-    return(
-        <p>Hello</p>
-    )
+    return (
+        <div className="play-game">
+            <div className="profile-section">
+                <p>Patient Profile</p>
+                <img className='patientImage' src={femalePatientAsset} alt='female-avatar' />
+            </div>
+            <div className="information-section">
+                <p><b>Age:</b> 42</p>
+                <p><b>Occupation:</b> Park Ranger</p>
+                <p><b>Background:</b></p>
+                <p>
+                    Sam spends most days outdoors, leading hikes and educational tours. Recently, they've been experiencing short, involuntary intakes of breath that sound like whispers, causing concern among tour groups. Sam has also noted a decrease in physical stamina and occasional lightheadedness, particularly after rapid movements or longer hikes.
+                </p>
+            </div>
+        </div>
+    );
 }
 
 export default Game;
