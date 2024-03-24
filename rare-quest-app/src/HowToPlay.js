7f5837194d175e1a6e8f717efa89400301ce63b3
Tahsin-Islam <tahsin_i12@outlook.com>
2024-03-24 12:38:16 -0400

    Added instructions to HowToPlay

--- rare-quest-app/src/HowToPlay.js
+++ rare-quest-app/src/HowToPlay.js
@@ -2,19 +2,31 @@ import './css/HowToPlay.css'
 
 function HowToPlay() {
     return(
-        <div>
+        <div className='instructions'>
             <h1>HOW TO PLAY</h1>
             <ol>
-                <li><b>Meet Your Patient</b><br></br>
+                <li className='instruction-step'><b>Meet Your Patient</b><br></br>
                     You'll be introduced to a patient, complete with a background story and a list of symptoms. 
                     Pay close attention to the details – they're crucial for your decisions ahead.
                 </li>
-                <li><b>Conduct Research</b><br></br>
+                <li className='instruction-step'><b>Conduct Research</b><br></br>
                     Before making any treatment decisions, use the "Research" option to gather more information about the patient's condition. 
                     This will unlock new insights and treatment options, enhancing your ability to care for the patient effectively.
                 </li>
-                
+                <li className='instruction-step'><b>Select Healing Actions</b><br></br>
+                    Based on the symptoms and the information you've gathered, choose from a list of healing actions. 
+                    Each action has a percentage effectiveness, indicating how likely it is to improve the patient's condition.
+                </li>
+                <li className='instruction-step'><b>Observe and Adjust</b><br></br>
+                    After selecting a treatment, observe its impact on the patient's wellness. 
+                    You can adjust your approach based on the patient's response and any new information you uncover through research.
+                </li>
+                <li className='instruction-step'><b>Progress Through the Game</b><br></br>
+                    Successfully managing one patient's care unlocks the next patient case. 
+                    Each new case presents unique challenges and learning opportunities.
+                </li>
             </ol>
+
         </div>
     )
 }
